bb4332c3f48b7df38e17aafdd3c79dcb49482ee9
michaelsimon960 <michaelsimon960@gmail.com>
2021-04-10 17:59:28 -0700

    added tables

--- EmployeeSQL/tables.sql
+++ EmployeeSQL/tables.sql
@@ -1,30 +1,40 @@
+-- Drop Tables if Existing
+DROP TABLE IF EXISTS employees CASCADE;
+DROP TABLE IF EXISTS departments CASCADE;
+DROP TABLE IF EXISTS titles CASCADE;
+DROP TABLE IF EXISTS dept_emp CASCADE;
+DROP TABLE IF EXISTS salaries CASCADE;
+DROP TABLE IF EXISTS dept_manager CASCADE;
+
 -- Create employees table
+
 create TABLE employees (
 	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title VARCHAR(35) NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(35) NOT NULL,
 	last_name VARCHAR(35) NOT NULL,
 	sex VARCHAR(35) NOT NULL,
+	hire_date DATE NOT NULL
 );
 
 -- Create departments table
 create TABLE departments (
 	dept_no VARCHAR(35) PRIMARY KEY NOT NULL,
-	dept_name VARCHAR(35) NOT NULL,
+	dept_name VARCHAR(35) NOT NULL
 );
 
 -- Create titles table
 create TABLE titles (
 	emp_no INT NOT NULL,
-	emp_title VARCHAR(35) NOT NULL,
-	FOREIGN KEY (emp_no) references employees (emp_no)
+	emp_title VARCHAR(35) NOT NULL
 );
 
 -- Create department employee table
 create TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(35) NOT NULL,
-	FOREIGN KEY (emp_no) references employees (emp_no)
+	FOREIGN KEY (emp_no) references employees (emp_no),
 	FOREIGN KEY (dept_no) references departments (dept_no)
 );
 
@@ -40,5 +50,8 @@ create TABLE dept_manager (
 	dept_no VARCHAR(35) NOT NULL,
 	emp_no INT NOT NULL,
 	FOREIGN KEY (dept_no) references departments (dept_no),
-	FOREIGN KEY (emp_no) references employees (emp_no),
+	FOREIGN KEY (emp_no) references employees (emp_no)
 );
+
+-- View the tables to verify imports
+Select * FROM employees
